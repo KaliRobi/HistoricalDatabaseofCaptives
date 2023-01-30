a8ce668429788e97b43d7b55932581cf7e52d60e
robi <kali_robi@hotmail.com>
2023-01-30 21:09:03 +0200

    first commit

---
+++ src/main/java/projectH/HistoricalDatabaseofCaptives/HistoricalDatabaseofCaptivesApplication.java
@@ -0,0 +1,13 @@
+package projectH.HistoricalDatabaseofCaptives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalDatabaseofCaptivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalDatabaseofCaptivesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectH/HistoricalDatabaseofCaptives/HistoricalDatabaseofCaptivesApplicationTests.java
@@ -0,0 +1,13 @@
+package projectH.HistoricalDatabaseofCaptives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricalDatabaseofCaptivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
