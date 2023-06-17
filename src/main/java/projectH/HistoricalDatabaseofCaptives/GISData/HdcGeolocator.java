1a711a29ede3c9a445bd1470dc7c51120752a18a
robi <kali_robi@hotmail.com>
2023-06-18 00:00:44 +0300

    Adding Haversine formula

--- src/main/java/projectH/HistoricalDatabaseofCaptives/GISData/HdcGeolocator.java
+++ src/main/java/projectH/HistoricalDatabaseofCaptives/GISData/HdcGeolocator.java
@@ -110,10 +110,39 @@ public class HdcGeolocator {
 
     //validate the distance from Budapest to check if the returned lat/lon data is ok
 
+    private double getDistanceBetween(GeologicalLocations locationA, GeologicalLocations locationB ){
+//        convert to Radian calc = degree * 3.1415926535 / 180;
+        double latA = Math.toRadians(locationA.getLatitude());
+        double lonA = Math.toRadians(locationA.getLongitude());
+        double latB = Math.toRadians( locationB.getLatitude());
+        double lonB = Math.toRadians( locationB.getLongitude());
+
+        double radiusOfEarth = 6371000;
+        double difLat = latB - latA;
+        double difLon = lonB - lonA;
+//        math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
+        double sinatan2Base = Math.sin(difLat / 2) * 2 + Math.cos(latA) * Math.cos(latB) * Math.sin(difLon);
+        return 0;
+    }
 //    Haversine formula
+//    phi_1 = math.radians(lat1)
+//    phi_2 = math.radians(lat2)//
+//    delta_phi = math.radians(lat2 - lat1)
+//    delta_lambda = math.radians(lon2 - lon1)
+//
+//    a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
+//
+//    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+//
+//    meters = R * c  # output distance in meters
+//    km = meters / 1000.0  # output distance in kilometers
+//
+//    meters = round(meters, 3)
+//    km = round(km, 3)
+
 
 //    D= 3440.1 * arccos[(sin (latA) * sin(lat B)) + cos(lat A ) * cos(lat B) * cos(lat A - long B)] everything is expressed in radians
-    // radiance = xπ / 180
+    // radian = xπ / 180
 
     //tasks
 
