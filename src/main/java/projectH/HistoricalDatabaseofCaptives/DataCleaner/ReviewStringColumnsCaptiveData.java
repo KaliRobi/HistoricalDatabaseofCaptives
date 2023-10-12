7356a1c1bba99c6055569a6b4fc14020fe5bce99
robi <kali_robi@hotmail.com>
2023-10-12 22:05:17 +0300

    minimal time, minimal progress

--- src/main/java/projectH/HistoricalDatabaseofCaptives/DataCleaner/ReviewStringColumnsCaptiveData.java
+++ src/main/java/projectH/HistoricalDatabaseofCaptives/DataCleaner/ReviewStringColumnsCaptiveData.java
@@ -7,30 +7,50 @@ instead of column specific reviews instance specific ones are mor effective.
  */
 
 import org.springframework.stereotype.Component;
+import projectH.HistoricalDatabaseofCaptives.CaptivesData.Captive;
+import projectH.HistoricalDatabaseofCaptives.CaptivesData.CaptiveServices;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class ReviewStringColumnsCaptiveData {
 
 
     private final LocalAbbreviatedEntityRepository localAbbreviatedEntityRepository;
+    private final CaptiveServices captiveServices;
+    private final CreateReviewableEntity createReviewableEntity;
 
-    public ReviewStringColumnsCaptiveData(LocalAbbreviatedEntityRepository localAbbreviatedEntityRepository) {
+    public ReviewStringColumnsCaptiveData(LocalAbbreviatedEntityRepository localAbbreviatedEntityRepository, CaptiveServices captiveServices, CreateReviewableEntity createReviewableEntity) {
         this.localAbbreviatedEntityRepository = localAbbreviatedEntityRepository;
+        this.captiveServices = captiveServices;
+        this.createReviewableEntity = createReviewableEntity;
     }
 
      // need  a switch which loops though the keys (tables) and each table will loops though the columns
 
     public void goForIt(){
         Map<String, List<String>> ValidatingString  =  getListOfTablesUsingAbbr();
-
+        reviewColumns();
 
     }
-// add custom queries for all the columns
+    //TODO
+    // The thing is that there are like 20 similar cases when I do captiveServices.getAllTheCaptives().stream()
+    // This should be just stored in a cache slot
+    // So next project is to get back the cache part and implement it. Also, will check for further similar things.
+ public void reviewColumns(){
+     List<Captive> captiveList = captiveServices.getAllTheCaptives();
+
+
+     Map<Long, Integer> femaleIdHeightMap = captiveList.stream().filter(e -> e.getSex().equals("n"))
+             .collect(Collectors.toMap(Captive::getId, Captive::getHeight));
+ }
+
 
 
     // get the concept of id:entity fom the height
+
+
 //     compare the db to the lists of ValidatingString
 
     // where something is wrong it goes to the reviewable table]
